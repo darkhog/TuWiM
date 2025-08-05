95940a0b8a760baccbf077d9541146eb553e2afb
D. G. Jagielski <rave002@o2.pl>
2025-08-05 16:28:21 +0200

    Basic error checking
    Added basic error checking so the launcher won't fry the computer.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 //licensed under GPL
 //The main file.
 
+#define NULLDISPLAYERR 230
 #include <X11/Xlib.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,9 +10,17 @@
 #include <dirent.h>
 
 int main(int argc, char* argv[]){
-	Display *dspl;
+	Display *dspl = NULL;
+	printf("Opening X Display...\n");
 	dspl = XOpenDisplay(NULL);
-
+	if (dspl == NULL) {
+		printf("Error: No valid X display provided. Are you trying to run this on Wayland? You poor silly goose...\n");
+		return NULLDISPLAYERR;
+	} else {
+		printf("Successfully opened X Display.\n");
+	}
+	printf("Closing the X Display...\n");
 	XCloseDisplay(dspl);
+	printf("Display closed. Exiting gracefully.\n");
 	return 0;
 }
