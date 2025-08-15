b74f9aca61cf5b0b0cfe1e2a72170c695625c904
D. G. Jagielski <rave002@o2.pl>
2025-08-15 18:18:51 +0200

    Post stream commit
    I am streaming the development of TuWiM on my kick channel https://kick.com/ultimateperfection
    
    Things done:
    - Implemented basic implementation of the loading window
    - Learned some more C. Soon I will be able to C clearly.

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 //licensed under GPL
 //The main file.
 
+#include <X11/X.h>
 #define NULLDISPLAYERR 230
+#define NULLROOTWNDERR 86
 #include <X11/Xlib.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -40,13 +42,40 @@ int main(int argc, char* argv[]){
 	} else {
 		printf("Successfully opened X Display.\n");
 	}
+
+	Window rootWnd;
+	printf("Attempting tp get the root window...");
+	rootWnd =XDefaultRootWindow(dspl);
+	/*if (rootWnd == NULL){
+		printf("Error: No valid X root window found. Are you trying to run this on Wayland? You poor silly goose...\n");
+		return NULLROOTWNDERR;
+	} else {
+		printf("Root window got successfully!");
+	}*/
+	int defScr = DefaultScreen(dspl);
 	/*
 	 +--------------------------------------+
 	 | TuWiM is scanning desktop entries... |
 	 +--------------------------------------+
 	*/
-	Window *ldWnd = NULL; //Window shown during the desktop entry scanning process to let the user know that the program is doing something
-	Window *mainWnd = NULL; //main window of the launcher.
+	Window ldWnd; //Window shown during the desktop entry scanning process to let the user know that the program is doing something
+	printf("Attempting to create loading window");
+	int wH =0; int wW=0; //window width/height
+	wH = DisplayHeight(dspl, defScr)/6;
+	wW = DisplayWidth(dspl, defScr)/4;
+	XSetWindowAttributes ldWndAttr;
+	ldWndAttr.background_pixel = 0;
+	ldWndAttr.colormap = XCreateColormap(dspl, rootWnd, DefaultVisual(dspl, defScr), AllocNone);
+	ldWnd = XCreateWindow(dspl,rootWnd,(DisplayWidth(dspl, defScr)/2)-(wW/2), (DisplayHeight(dspl, defScr)/2)-(wH/2), wW, wH, 0, CopyFromParent,InputOutput,CopyFromParent,CWBackPixel | CWColormap, &ldWndAttr);
+	for (;;){
+		//loading
+	}
+	XStoreName(dspl,ldWnd,"Loading TuWiM...");
+	XMapWindow(dspl,ldWnd);
+	XFlush(dspl);
+	Window categoryWnd; //main window of the launcher.
+	Window itemWnd;
+
 	printf("Closing the X Display...\n");
 	XCloseDisplay(dspl);
 
