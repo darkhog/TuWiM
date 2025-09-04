0885bb0d7bc8a8551113891291825b1bc982ac60
D. G. Jagielski <rave002@o2.pl>
2025-09-04 18:15:43 +0200

    Globalized some variables as well as shufled code into logically separated functions
    It is important for the code quality and the future maintainability of TuWiM.

--- main.c
+++ main.c
@@ -11,6 +11,61 @@
 #include <string.h>
 #include <dirent.h>
 
+//we need to globalize certain variables to write the code in more maintainable way instead of one giant main()
+Display *dspl;// XDisplay
+Window rootWnd; //X11 root window
+int defScr; //default screen of display.
+Window ldWnd; //Window shown during the desktop entry scanning process to let the user know that the program is doing something
+Window categoryWnd; //main window of the launcher.
+Window itemWnd; //individual item window/category subwindow
+
+
+int initX11() {
+	dspl = NULL;
+	printf("Opening X Display...\n");
+	dspl = XOpenDisplay(NULL);
+
+	if (dspl == NULL) {
+		printf("Error: No valid X display provided. Are you trying to run this on Wayland? You poor silly goose...\n");
+		return NULLDISPLAYERR;
+	} else {
+		printf("Successfully opened X Display.\n");
+	}
+
+
+	printf("Attempting tp get the root window...");
+	rootWnd =XDefaultRootWindow(dspl);
+	/*if (rootWnd == NULL){
+	 p rintf("Error: No valid X root window found. Are you trying to run this on Wayland? You poor silly goose...\n");                                                                                            *
+	 return NULLROOTWNDERR;
+} else {
+	printf("Root window got successfully!");
+}*/
+	defScr = DefaultScreen(dspl);
+	return 0;
+}
+
+void initLoadingWindow(){
+	/*
+	 +--------------------------------------+
+	 | TuWiM is scanning desktop entries... |
+	 +--------------------------------------+
+	 */
+
+	printf("Attempting to create loading window");
+	int wH =0; int wW=0; //window width/height
+	wH = DisplayHeight(dspl, defScr)/6;
+	wW = DisplayWidth(dspl, defScr)/4;
+	XSetWindowAttributes ldWndAttr;
+	ldWndAttr.background_pixel = 0;
+	ldWndAttr.colormap = XCreateColormap(dspl, rootWnd, DefaultVisual(dspl, defScr), AllocNone);
+	ldWnd = XCreateWindow(dspl,rootWnd,(DisplayWidth(dspl, defScr)/2)-(wW/2), (DisplayHeight(dspl, defScr)/2)-(wH/2), wW, wH, 0, CopyFromParent,InputOutput,CopyFromParent,CWBackPixel | CWColormap, &ldWndAttr);
+	XStoreName(dspl,ldWnd,"Loading TuWiM...");
+	XMapWindow(dspl,ldWnd);
+	XFlush(dspl);
+	return;
+}
+
 int main(int argc, char* argv[]){
 	if (argc>1) {
 		if (strcmp(argv[1],"dragons")==0){
@@ -32,49 +87,16 @@ int main(int argc, char* argv[]){
 		}
 	}
 
-	Display *dspl = NULL;
-	printf("Opening X Display...\n");
-	dspl = XOpenDisplay(NULL);
-
-	if (dspl == NULL) {
-		printf("Error: No valid X display provided. Are you trying to run this on Wayland? You poor silly goose...\n");
-		return NULLDISPLAYERR;
-	} else {
-		printf("Successfully opened X Display.\n");
+	int Xerr = initX11();
+	if (Xerr!=0){
+		return Xerr;
 	}
-
-	Window rootWnd;
-	printf("Attempting tp get the root window...");
-	rootWnd =XDefaultRootWindow(dspl);
-	/*if (rootWnd == NULL){
-		printf("Error: No valid X root window found. Are you trying to run this on Wayland? You poor silly goose...\n");
-		return NULLROOTWNDERR;
-	} else {
-		printf("Root window got successfully!");
-	}*/
-	int defScr = DefaultScreen(dspl);
-	/*
-	 +--------------------------------------+
-	 | TuWiM is scanning desktop entries... |
-	 +--------------------------------------+
-	*/
-	Window ldWnd; //Window shown during the desktop entry scanning process to let the user know that the program is doing something
-	printf("Attempting to create loading window");
-	int wH =0; int wW=0; //window width/height
-	wH = DisplayHeight(dspl, defScr)/6;
-	wW = DisplayWidth(dspl, defScr)/4;
-	XSetWindowAttributes ldWndAttr;
-	ldWndAttr.background_pixel = 0;
-	ldWndAttr.colormap = XCreateColormap(dspl, rootWnd, DefaultVisual(dspl, defScr), AllocNone);
-	ldWnd = XCreateWindow(dspl,rootWnd,(DisplayWidth(dspl, defScr)/2)-(wW/2), (DisplayHeight(dspl, defScr)/2)-(wH/2), wW, wH, 0, CopyFromParent,InputOutput,CopyFromParent,CWBackPixel | CWColormap, &ldWndAttr);
+	initLoadingWindow();
 	for (;;){
 		//loading
 	}
-	XStoreName(dspl,ldWnd,"Loading TuWiM...");
-	XMapWindow(dspl,ldWnd);
-	XFlush(dspl);
-	Window categoryWnd; //main window of the launcher.
-	Window itemWnd;
+
+
 
 	printf("Closing the X Display...\n");
 	XCloseDisplay(dspl);
