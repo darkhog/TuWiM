e3f0559c77d1546ac15655dcb9815f29765cced4
D. G. Jagielski <rave002@o2.pl>
2026-01-04 12:48:46 +0100

    Fixed build script so it actually, you know, work
    Heh.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-gcc -Wall -o "./tuwim" "../main.c" -lX11
+#even if the directory exists, it will just exit with a harmless error
+#no need to test for its existence
+#and if you have a file called "build" here it's your own damn fault.
+mkdir build > /dev/null
+gcc -Wall -o "./build/tuwim" "./main.c" -lX11
